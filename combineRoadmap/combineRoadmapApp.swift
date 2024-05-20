8274df54ad2fd00614d9b7c4c58d140143462999
ad <ad>
2024-05-20 22:59:47 +0500

    Initial Commit

---
+++ combineRoadmap/combineRoadmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  combineRoadmapApp.swift
+//  combineRoadmap
+//
+//  Created by Adlet Zhantassov on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct combineRoadmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
